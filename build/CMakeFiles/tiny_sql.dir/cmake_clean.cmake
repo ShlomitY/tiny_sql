92d05c17ce8bdc5c0bb39a0e6e8093647583a342
ShlomitY <129608543+ShlomitY@users.noreply.github.com>
2026-07-10 17:22:21 +0300

    changed layout

---
+++ build/CMakeFiles/tiny_sql.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tiny_sql.dir/src/column.cpp.obj"
+  "CMakeFiles/tiny_sql.dir/src/column.cpp.obj.d"
+  "CMakeFiles/tiny_sql.dir/src/file_proccessing.cpp.obj"
+  "CMakeFiles/tiny_sql.dir/src/file_proccessing.cpp.obj.d"
+  "CMakeFiles/tiny_sql.dir/src/main.cpp.obj"
+  "CMakeFiles/tiny_sql.dir/src/main.cpp.obj.d"
+  "CMakeFiles/tiny_sql.dir/src/table.cpp.obj"
+  "CMakeFiles/tiny_sql.dir/src/table.cpp.obj.d"
+  "libtiny_sql.dll.a"
+  "tiny_sql.exe"
+  "tiny_sql.exe.manifest"
+  "tiny_sql.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tiny_sql.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+#include "../include/table.h"
+
+
+using namespace std;
+
+void devider(const string& command, vector<string>& words);
+int handle(vector<string>& words, int number_command, vector<Table*>& tables, std::ofstream& UnsuccessfulFile , std::ofstream& SelectOutputFile);
+void printTables(const vector<Table*> &tables, std::ofstream& file, int command);
+
+//TODO: change these next lines.
+int main() {
+	vector<Table*> tables;
+	ifstream command_file("../Input-output/Input/input.txt");
+	std::ofstream CurrentTablesFile("../Input-output/Output/CurrentTables.txt");
+	std::ofstream SelectOutputFile("../Input-output/Output/SelectOutput.txt");
+	std::ofstream UnsuccessfulFile("../Input-output/Output/Unsuccessful.txt");
+
+	if (!UnsuccessfulFile) {
+		cout << "failed to open the input commands file!" << endl;
+		cout << "Remainder: the commands file should be located in the Input-output\\Input folder and should be named \"input.txt\" ";
+		return -1;
+	}
+
+	if (!SelectOutputFile) {
+		UnsuccessfulFile << "failed to open the input commands file!" << endl;
+		UnsuccessfulFile << "Remainder: the commands file should be located in the Input-output\\Input folder and should be named \"input.txt\" ";
+		return -1;
+	}
+
+
+	if (!command_file) {
+		UnsuccessfulFile << "failed to open the input commands file!" << endl;
+		UnsuccessfulFile << "Remainder: the commands file should be located in the Input-output\\Input folder and should be named \"input.txt\" ";
+		return -1;
+	}
+	int number_command = 0;
+
+	string current_command;
+	while(getline(command_file, current_command)) {
+		if (current_command.size() < 2) {
+			continue;
+		}
+		number_command++;
+		vector<string> words;
+		devider(current_command, words);
+		int ret = handle(words, number_command, tables, UnsuccessfulFile, SelectOutputFile);
+		if (ret == 1) {
+			continue;
+		}
+		if (ret == 2) {
+			return 1;
+		}
+
+		if (CurrentTablesFile.is_open()) {
+			printTables(tables, CurrentTablesFile, number_command);
+		} else {
+			UnsuccessfulFile << "Failed to open file for writing." << endl;
+		}
+
+
+	}
+	CurrentTablesFile.close();
+	command_file.close();
+	return 0;
+}
